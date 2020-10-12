96d3f1ac33b0fe2655d0a17a8691846130d0d1e2
ali <ali.haydaroglu@mail.utoronto.ca>
2020-10-12 01:09:36 +0100

    ali's message

---
+++ msgs/Ali/msg.py
@@ -0,0 +1,49 @@
+'''
+    sample code. PLEASE KEEP THE NAME OF THIS FILE AS msg.py
+
+    You may run python3 msg.py to test this function. Ignore the moving cursor; it will not appear in the actual display.
+
+'''
+## INCLUDE THIS PART
+import sys
+sys.path.append("../../")
+from utils.lcd import Message, display_test
+
+## DO NOT CHANGE THE NAME OF THIS FUNCTION. This will be imported to the main code
+def get_msg_list():
+    '''
+        This function should return a list of Message objects you created.
+
+        Our screen resolution is 2x16 pixels.
+
+        Note that every Message object takes in 3 arguments:
+            - line_one: a string no longer than 16 characters, specifying what to be displayed on first row
+            - line_two: a string no longer than 16 characters, specifying what to be displayed on second row
+            - duration: a number specifying the duration of display in seconds
+    '''
+    msg_list = []
+    ########################
+    ## CUSTOMISATION START #
+    ########################
+    # you can create messages like this
+    msg_list.append( Message("-----HELLO!-----", "================", 1) )
+    msg_list.append( Message("Thank you", "for LBB!", 1) )
+    msg_list.append( Message("Here's a", "scared bot:", 1))
+
+    for i in range(10):
+        msg_list.append(Message (" ", " " * i + ".=>.", 0.1))
+    msg_list.append(Message (" " * i + "*****"," "*i + ".=>.", 0.5))
+    msg_list.append(Message (" " * i + "*****", " "* 7 + "!!.=>.", 0.3))
+    for i in range(10): 
+        msg_list.append(Message (" "*10 + "*****", " "*(9-i) + ".=>.", 0.1)) 
+    
+    msg_list.append(Message ("- Ali", " ", 0.8))
+
+    ########################
+    ## CUSTOMISATION END #
+    ########################
+    return msg_list
+
+if __name__ == "__main__":
+    ## THIS TESTS YOUR MESSAGES ON COMMAND LINE
+    display_test(get_msg_list())
